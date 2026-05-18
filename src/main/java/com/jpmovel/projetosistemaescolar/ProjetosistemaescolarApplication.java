1c9966f64bfdf1eab326d7f7f13d4589a041fa42
Cauan Nascimento <caunas1337@proton.me>
2026-05-17 22:34:57 -0300

    add: Project setup

---
+++ src/main/java/com/jpmovel/projetosistemaescolar/ProjetosistemaescolarApplication.java
@@ -0,0 +1,13 @@
+package com.jpmovel.projetosistemaescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetosistemaescolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetosistemaescolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpmovel/projetosistemaescolar/ProjetosistemaescolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpmovel.projetosistemaescolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosistemaescolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
