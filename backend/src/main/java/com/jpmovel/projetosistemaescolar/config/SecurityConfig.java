dbf9f8265760efb8e304b53cb0dee9b1ac1b33d1
Cauan Nascimento <caunas1337@proton.me>
2026-06-15 03:36:27 -0300

    hotfix: Config do CORS da API para consultas externas

--- backend/src/main/java/com/jpmovel/projetosistemaescolar/config/SecurityConfig.java
+++ backend/src/main/java/com/jpmovel/projetosistemaescolar/config/SecurityConfig.java
@@ -15,6 +15,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> {}) // sem isso, frontend nn consegue consummir a api
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         // Público
